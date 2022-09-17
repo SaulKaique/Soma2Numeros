7dee256d9bf7fe7ea955236dd9af92bad415ffb8
saulkaique <saul.silva@etec.sp.gov.br>
2022-09-17 11:06:07 -0300

    bom terminei a soma de dois numeros

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int numero1, numero2, soma;
+
+Console.WriteLine("Caulo de Soma de dois Numero. \n ");
+
+Console.Write("Digite o primero Numero: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Digite o primero Numero: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+
+soma = numero1 + numero2;
+
+Console.Write($"\nsoma: {soma}");
